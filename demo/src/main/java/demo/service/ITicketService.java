e43b058b127aef1f1ca838b4ab0df9ba718827ad
unknown <hgno3001@gmail.com>
2022-12-05 08:36:46 +0700

    BE-exam

---
+++ demo/src/main/java/demo/service/ITicketService.java
@@ -0,0 +1,16 @@
+package demo.service;
+
+import demo.model.Ticket;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITicketService {
+    List<Ticket> search(String departure, String destination, String date, String time);
+
+    Optional<Ticket> findById(int id);
+
+    void save(Ticket ticket);
+
+    List<Ticket> findAndSearch(String departure, String destination);
+}
